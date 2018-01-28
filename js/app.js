24ab175dbd69d25d406e934ecb919db80d51f2ca
annakubera <kuberaanna@gmail.com>
2018-01-28 13:20:59 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var user = "Ania";
+
+$("input").on("keypress", function(event) {
+  //event.preventDefault();
+  //console.log(event.keyCode);
+  if(event.keyCode === 13) {
+    var message = event.target.value;
+    
+    sendMessage(message);
+    renderMessage(message, user);
+    $(this).val('');
+  }
+});
+
+function sendMessage(message) {
+  console.log("Wysyłam " + message);
+}
+
+function renderMessage(message, user) {
+  var date = new Date();
+  var year = date.getFullYear();
+  
+  $("#messagePanel").append(
+    "<p class='user'>" + user + "</p>" + 
+    "<p>" + message + "</p>" +
+    "<p class='date'>" + year + "</p>"
+  );
+  
+}
