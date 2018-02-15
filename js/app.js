41a86e8bd598e6762a52a7959035954f45405bd1
annakubera <kuberaanna@gmail.com>
2018-02-15 21:08:13 +0100

    added function renderMessages

--- js/app.js
+++ js/app.js
@@ -1,33 +1,57 @@
 var user = "Ania";
+var messageData = [{
+  user: "Pawel",
+  text: "Hello"
+}, {
+  user: "Ania",
+  text: "Czesc"
+}, {
+  user: "Ania",
+  text: "Co tam?"
+}, {
+  user: "Pawel",
+  text: "Nic! Wszystko spoko, jakoś leci... :)"
+}];
+
+renderMessages(messageData);
 
 $("input").on("keypress", function(event) {
   if(event.keyCode === 13) {
-    var message = event.target.value;
+    var text = event.target.value;
     
-    sendMessage(message);
-    renderMessage(message, user);
+    sendMessage(text);
+    renderMessage(text, user);
     scrollToBottom();
     $(this).val('');
   }
 });
 
-function sendMessage(message) {
-  console.log("Wysyłam " + message);
+// Prints the content of var text to the console 
+function sendMessage(text) {
+  console.log("Wysyłam " + text);
 }
 
-function renderMessage(message, user) {
+// Renders into the messagePanel all the messages written by the user into the input 
+function renderMessage(text, user) {
   var date = new Date();
   var year = date.getFullYear();
   
   $("#messageWrapper").append(
     "<div class='bubble'><p class='user'>" + user + "</p>" + 
-    "<p class='message'>" + message + "</p>" +
+    "<p class='text'>" + text + "</p>" +
     "<p class='date'>" + year + "</p></div>"
-  );
-  
+  ); 
 }
 
+// Scrolls all messages to the very last on bottom
 function scrollToBottom() {
   var messageWrapperHeight = $("#messageWrapper").height(); 
   $("#messagePanel").scrollTop(messageWrapperHeight);
+}
+
+// (At the start of the page) Renders all messages from array of var messageData
+function renderMessages() {
+  messageData.forEach(function (message) {
+    renderMessage(message.text, message.user);
+  });
 }
